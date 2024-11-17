73dcce53832092215270b7f3a6f8b52a2605445d
dtessler2 <154753370+dtessler2@users.noreply.github.com>
2024-11-17 17:26:42 -0500

    added the interface for the random value generating class

---
+++ src/assignment_4_Final/IRandomValueGenerator.java
@@ -0,0 +1,7 @@
+package assignment_4_Final;
+
+public interface IRandomValueGenerator {
+
+	public int randNumInRange(int min, int max);
+	public boolean getTrueWithProbability(double probability);
+}
