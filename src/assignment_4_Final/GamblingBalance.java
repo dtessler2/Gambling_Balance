780f55eed4c632562a0873342af253dd2375aeca
dtessler2 <154753370+dtessler2@users.noreply.github.com>
2024-11-17 19:09:02 -0500

    Added draft of GamblingBalance

---
+++ src/assignment_4_Final/GamblingBalance.java
@@ -0,0 +1,33 @@
+package assignment_4_Final;
+public class GamblingBalance {
+	private double balance;
+	private double minBalance;
+	IRandomValueGenerator rv;
+	
+	public GamblingBalance(double minBalance, IRandomValueGenerator rv) {
+		
+	}
+	
+	public double getCurrentBalance() {
+		return 0;
+	}
+	
+	public boolean canBet(double amnt) {
+		return false;
+	}
+	
+	public void addMoney(double amnt) {
+		
+	}
+	
+	public double betOnANumber(double amnt, int min, int max, int selectedNumber) {
+		return 0;
+	}
+	
+	
+	public double betOnProbability(double amnt, double p) {
+		return 0;
+	}
+	
+}
+
